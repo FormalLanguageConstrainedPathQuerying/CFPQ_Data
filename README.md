d89dd5b6f722e6a91801641e7e77279da9f7e521
azaat <a.r.akhmetyanov@gmail.com>
2020-03-29 21:25:52 +0300

    Add reference values and more links

--- README.md
+++ README.md
@@ -46,46 +46,47 @@ Work in progress.
 
 ## Data set
 
-Set contains both real-world data and synthetic graphs for several specific cases; all graphs are represented in triples.
-All graphs are represented in RDF format to unify loading process.
+Set contains both real-world data and synthetic graphs for several specific cases; all graphs are represented in RDF format to unify loading process.
 
 ### Graphs
-- **RDF** — Real-world RDF files:
 
-  - Smaller graphs
-    - a set of popular semantic web ontologies like **foaf**, **wine**, **pizza**.
-  - Bigger graphs
-    - **geospecies** – graph related to taxonomic hierarchy and geographical information of animal species
-    - a set of graphs from the **Uniprot** protein sequences database
+```data/Matrices/RDF``` — Fixed versions of real-world RDF files(links are provided for updating purposes only!):
 
-- **Worst cases** — graphs with two cylces; the query is a grammar for the language of correct bracket sequences.
+   - Smaller graphs:
+    - a set of popular semantic web ontologies, download links: [skos](https://www.w3.org/2009/08/skos-reference/skos.rdf), [foaf](http://xmlns.com/foaf/0.1/), [wine](https://www.w3.org/TR/owl-guide/wine.rdf), [pizza](https://protege.stanford.edu/ontologies/pizza/pizza.owl), [generations](http://www.owl-ontologies.com/generations.owl), [travel](https://protege.stanford.edu/ontologies/travel.owl), [univ-bench](http://swat.cse.lehigh.edu/onto/univ-bench.owl), [people-pets](http://owl.man.ac.uk/tutorial/people+pets.rdf)
+  
+  - Bigger graphs:
+    - _geospecies_ – graph related to taxonomic hierarchy and geographical information of animal species, download here: <https://old.datahub.io/dataset/geospecies> 
+    - a set of graphs from the _Uniprot_ protein sequences database, download here: <ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/rdf>
 
-- **Sparse** — graphs generated with [GTgraph](http://www.cse.psu.edu/~kxm85/software/GTgraph/) to emulate sparse data.
+```data/Synthetic/Matrices/WorstCase``` — graphs with two cylces; the query is a grammar for the language of correct bracket sequences.
 
-- **Scale-free** — graphs generated with [GTgraph](http://www.cse.psu.edu/~kxm85/software/GTgraph/) by using the Barab\'asi-Albert model of scale-free networks
+```data/Synthetic/Matrices/SparseGraph``` — graphs generated with [GTgraph](http://www.cse.psu.edu/~kxm85/software/GTgraph/) to emulate sparse data.
 
-- **Full graph**
+```data/Synthetic/Matrices/ScaleFree``` — graphs generated with [GTgraph](http://www.cse.psu.edu/~kxm85/software/GTgraph/) by using the Barab\'asi-Albert model of scale-free networks
+
+```data/Synthetic/Matrices/FullGraph``` — a cycle, all edges are labeled with the same token 
 
 ### Grammars
 
-- **GPPerf1, GPPerf2** — queries over _subClassOf_ and _type_ relations 
-  - Use with **RDF** graphs
+```GPPerf1```, ```GPPerf2``` — queries over _subClassOf_ and _type_ relations 
+  - Use with _RDF_ dataset
 
-- **geo**
-  - Use with **geospecies** dataset
+```geo```
+  - Use with _geospecies_ dataset
 
-- **an_bm_cm_dn**
-  - Use with **ScaleFree** graphs
+```an_bm_cm_dn``` — query for _A<sub>n</sub>B<sub>m</sub>C<sub>m</sub>D<sub>n</sub>_ language
+  - Use with _ScaleFree_ graphs
 
-- **Brackets** — query describing correct bracket sequences
-  - Use with **WorstCase** graphs
+```Brackets``` — query describing correct bracket sequences
+  - Use with _WorstCase_ graphs
 
-- **A_star** — kleene star producing full graph
-  - Use with **FullGraph**
+```A_star``` — kleene star query producing full graph
+  - Use with _FullGraph_
 
 ### Reference values
 
-Control values(e.g. number of paths) for algorithms checking: Work in progress
+Control values for algorithms correctness checking can be found in ```control_values.csv```.
 
 ## Papers using this data set
 
--- init.py
+++ init.py
@@ -9,16 +9,27 @@ import argparse
 import numpy as np
 from rdflib import Graph, URIRef, BNode
 
-from contextlib import closing
-
 GT_GRAPH_URL = 'http://www.cse.psu.edu/~kxm85/software/GTgraph/GTgraph.tar.gz'
 GT_GRAPH_ARCH = './tools/GTgraph.tar.gz'
 
 MEMORY_ALIASES_DOWNLOAD_ID = '1fVMY1rE7vX-bFWdP3CDTdjILDsELOXK0'
 RDF_DOWNLOAD_ID = '1x4cELJ7kSwhqlLC0zrOcuxaF3c19qRnK'
 
-SPARSE_GRAPH_TO_GEN = [[5000, 0.001], [10000, 0.001], [10000, 0.01], [10000, 0.1], [20000, 0.001], [40000, 0.001], [80000, 0.001]]
-FULL_GRAPH_TO_GEN = [10, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 25000, 50000, 80000]
+SPARSE_GRAPH_TO_GEN = [
+    [5000, 0.001],
+    [10000, 0.001],
+    [10000, 0.01],
+    [10000, 0.1],
+    [20000, 0.001],
+    [40000, 0.001],
+    [80000, 0.001],
+]
+FULL_GRAPH_TO_GEN = [
+    10, 50, 100, 200,
+    500, 1000, 2000,
+    5000, 10000, 25000,
+    50000, 80000,
+]
 NUMBER_OF_WORST_CASES = 12
 URI_PREFIX = 'http://yacc/'
 RDF = 'RDF'
@@ -26,25 +37,27 @@ DATA_ROOT_DIR = './data/'
 SYNTHETIC_DIR = 'Synthetic'
 MATRICES_DIR = 'Matrices'
 
-#MEMORY_ALIASES = 'MemoryAliases'
-#DATA_TO_UNPACK = [[MEMORY_ALIASES,MEMORY_ALIASES_DOWNLOAD_ID], [RDF,RDF_DOWNLOAD_ID]]
-DATA_TO_UNPACK = [[RDF,RDF_DOWNLOAD_ID]]
+# MEMORY_ALIASES = 'MemoryAliases'
+# DATA_TO_UNPACK = [[MEMORY_ALIASES,MEMORY_ALIASES_DOWNLOAD_ID], [RDF, RDF_DOWNLOAD_ID]]
+DATA_TO_UNPACK = [[RDF, RDF_DOWNLOAD_ID]]
 GT_GRAPH = './tools/GTgraph/random/GTgraph-random'
 TMP_FILE = 'tmp.txt'
+
 
 def download_file_from_google_drive(id, destination):
     URL = 'https://docs.google.com/uc?export=download'
 
     session = requests.Session()
 
-    response = session.get(URL, params = { 'id' : id }, stream = True)
+    response = session.get(URL, params={'id': id}, stream=True)
     token = get_confirm_token(response)
 
     if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
+        params = {'id': id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
 
-    save_response_content(response, destination)    
+    save_response_content(response, destination)
+
 
 def get_confirm_token(response):
     for key, value in response.cookies.items():
@@ -53,13 +66,15 @@ def get_confirm_token(response):
 
     return None
 
+
 def save_response_content(response, destination):
     CHUNK_SIZE = 32768
 
     with open(destination, 'wb') as f:
         for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
+            if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
+
 
 def download_data():
     print('Downloading from GDrive is started.')
@@ -68,54 +83,76 @@ def download_data():
     for f in DATA_TO_UNPACK:
         arch_dst = os.path.join(dst, f[0] + '.tar.xz')
         print('Download archive to ' + arch_dst)
-        
-        download_file_from_google_drive(f[1], arch_dst)  
+
+        download_file_from_google_drive(f[1], arch_dst)
     print('Downloading from GDrive is finished.')
-    
+
+
 def unpack(file_from, path_to):
     with lzma.open(file_from) as f:
         with tarfile.open(fileobj=f) as tar:
             tar.extractall(path_to)
 
+
 def install_gtgraph():
     print('Installation of GTgraph is started.')
-    with urllib.request.urlopen(GT_GRAPH_URL) as response, open(GT_GRAPH_ARCH, 'wb') as out_file:
-       shutil.copyfileobj(response, out_file)
+    with urllib.request.urlopen(GT_GRAPH_URL) as response, open(
+        GT_GRAPH_ARCH, 'wb'
+    ) as out_file:
+        shutil.copyfileobj(response, out_file)
     print('GTgraph is downloaded.')
     tar = tarfile.open(GT_GRAPH_ARCH)
     tar.extractall('./tools')
     tar.close()
 
-    subprocess.run(['sed', '-i', 's/CC = icc/#CC = icc/g', './tools/GTgraph/Makefile.var'])
-    subprocess.run(['sed', '-i', 's/#CC = gcc/CC = gcc/g', './tools/GTgraph/Makefile.var'])
-    subprocess.run(['make', '-C', './tools/GTgraph']) 
-    print('Installation of GTgraph is finished.') 
+    subprocess.run(
+        ['sed', '-i', 's/CC = icc/#CC = icc/g', './tools/GTgraph/Makefile.var']
+    )
+    subprocess.run(
+        ['sed', '-i', 's/#CC = gcc/CC = gcc/g', './tools/GTgraph/Makefile.var']
+    )
+    subprocess.run(['make', '-C', './tools/GTgraph'])
+    print('Installation of GTgraph is finished.')
+
 
 def unpack_graphs():
     for d in DATA_TO_UNPACK:
         to = os.path.join(DATA_ROOT_DIR, MATRICES_DIR)
-        arch = os.path.join(to, '%s.tar.xz'%(d[0]))
-        print ('Unpack ', arch, ' to ', to)
+        arch = os.path.join(to, '%s.tar.xz' % (d[0]))
+        print('Unpack ', arch, ' to ', to)
         unpack(arch, to)
         os.rename(os.path.join(to, MATRICES_DIR), os.path.join(to, d[0]))
+
 
 # RDF serialization
 def write_to_rdf(target, graph):
     graph.serialize(target + '.xml', format='xml')
 
+
 def add_rdf_edge(subj, pred, obj, graph):
-    s = BNode('id-%s'%(subj))
+    s = BNode('id-%s' % (subj))
     p = URIRef(URI_PREFIX + pred)
-    o = BNode('id-%s'%(obj))
+    o = BNode('id-%s' % (obj))
     graph.add((s, p, o))
 
-def gen_sparse_graph(target_dir, vertices, prob):
- 
-   subprocess.run([GT_GRAPH, '-t', '0', '-n', '%s'%(vertices), '-p', '%s'%(prob), '-o', TMP_FILE])
 
-   with open(TMP_FILE) as in_file:
+def gen_sparse_graph(target_dir, vertices, prob):
+
+    subprocess.run([
+            GT_GRAPH,
+            '-t',
+            '0',
+            '-n',
+            '%s' % (vertices),
+            '-p',
+            '%s' % (prob),
+            '-o',
+            TMP_FILE,
+    ])
+
+    with open(TMP_FILE) as in_file:
         output_graph = Graph()
-        target = os.path.join(target_dir, 'G%sk-%s'%(int(vertices/1000), prob))
+        target = os.path.join(target_dir, 'G%sk-%s' % (int(vertices / 1000), prob))
         for l in in_file.readlines():
             if l.startswith('a '):
                 a = l.split(' ')
@@ -123,15 +160,16 @@ def gen_sparse_graph(target_dir, vertices, prob):
                 add_rdf_edge(a[1], lbl, a[2], output_graph)
         write_to_rdf(target, output_graph)
 
+
 def gen_worst_case_graph(target_dir, vertices):
     first_cycle = int(vertices / 2) + 1
     output_graph = Graph()
-    target = os.path.join(target_dir, 'worstcase_%s'%(vertices))
-    
+    target = os.path.join(target_dir, 'worstcase_%s' % (vertices))
+
     for i in range(0, first_cycle - 1):
         add_rdf_edge(i, 'A', i + 1, output_graph)
 
-    add_rdf_edge(first_cycle - 1, 'A', 0, output_graph)    
+    add_rdf_edge(first_cycle - 1, 'A', 0, output_graph)
     add_rdf_edge(first_cycle - 1, 'B', first_cycle, output_graph)
     for i in range(first_cycle, vertices - 1):
         add_rdf_edge(i, 'B', i + 1, output_graph)
@@ -140,9 +178,10 @@ def gen_worst_case_graph(target_dir, vertices):
 
     write_to_rdf(target, output_graph)
 
+
 def gen_cycle_graph(target_dir, vertices):
     output_graph = Graph()
-    target = os.path.join(target_dir, 'fullgraph_%s'%(vertices))
+    target = os.path.join(target_dir, 'fullgraph_%s' % (vertices))
 
     for i in range(0, vertices - 1):
         add_rdf_edge(i, 'A', i + 1, output_graph)
@@ -150,15 +189,15 @@ def gen_cycle_graph(target_dir, vertices):
 
     write_to_rdf(target, output_graph)
 
+
 def gen_scale_free_graph(target_dir, n, k, labels):
-    g = {
-        i: [(j, np.random.choice(labels))
-            for j in range(k)] for i in range(k)
-    }
+    g = {i: [(j, np.random.choice(labels)) for j in range(k)] for i in range(k)}
     degree = [3] * k
 
     for i in range(k, n):
-        to_vertices = np.random.choice(range(i), size=k, replace=False, p=np.array(degree) / sum(degree))
+        to_vertices = np.random.choice(
+            range(i), size=k, replace=False, p=np.array(degree) / sum(degree)
+        )
 
         g[i] = []
         degree.append(0)
@@ -167,118 +206,127 @@ def gen_scale_free_graph(target_dir, n, k, labels):
             g[i].append((to, label))
             degree[to] += 1
             degree[i] += 1
-    
+
     output_graph = Graph()
-    target = os.path.join(target_dir, 'scale_free_graph_%s_%s')%(n, k)
+    target = os.path.join(target_dir, 'scale_free_graph_%s_%s') % (n, k)
     for v in g:
         for to in g[v]:
             add_rdf_edge(v, to[1], to[0], output_graph)
-            
+
     write_to_rdf(target, output_graph)
 
+
 def clean_dir(path):
-    if os.path.isdir(path): 
+    if os.path.isdir(path):
         shutil.rmtree(path)
     os.mkdir(path)
 
-def generate_all_sparse_graphs():
-    print('Sparse graphs generation is started.') 
+
+def add_graph_dir(name):
     synthetic_dir = os.path.join(DATA_ROOT_DIR, SYNTHETIC_DIR, MATRICES_DIR)
     os.makedirs(synthetic_dir, exist_ok=True)
-    matrices_dir = os.path.join(synthetic_dir, 'SparseGraph')
+    matrices_dir = os.path.join(synthetic_dir, name)
     clean_dir(matrices_dir)
+    return matrices_dir
 
-    for g in SPARSE_GRAPH_TO_GEN: gen_sparse_graph(matrices_dir, g[0], g[1])
+
+def generate_all_sparse_graphs():
+    print('Sparse graphs generation is started.')
+    add_graph_dir('SparseGraph')
+    matrices_dir = add_graph_dir('SparseGraph')
+    for g in SPARSE_GRAPH_TO_GEN:
+        gen_sparse_graph(matrices_dir, g[0], g[1])
     print('Sparse graphs generation is finished.')
+
 
 def generate_full_graphs():
     print('Full graphs generation is started.')
-    synthetic_dir = os.path.join(DATA_ROOT_DIR, SYNTHETIC_DIR, MATRICES_DIR)
-    os.makedirs(synthetic_dir, exist_ok=True)
-    matrices_dir = os.path.join(synthetic_dir, 'FullGraph')
-    clean_dir(matrices_dir)
-
-    for g in FULL_GRAPH_TO_GEN: gen_cycle_graph(matrices_dir, g)
+    matrices_dir = add_graph_dir('FullGraph')
+    for g in FULL_GRAPH_TO_GEN:
+        gen_cycle_graph(matrices_dir, g)
     print('Full graphs generation is finished.')
+
 
 def generate_worst_case_graphs():
     print('Worst case graphs generation is started.')
-    synthetic_dir = os.path.join(DATA_ROOT_DIR, SYNTHETIC_DIR, MATRICES_DIR)
-    os.makedirs(synthetic_dir, exist_ok=True)
-    matrices_dir = os.path.join(synthetic_dir, 'WorstCase')
-    clean_dir(matrices_dir)
-
-    for n in range(2, NUMBER_OF_WORST_CASES): gen_worst_case_graph(matrices_dir, 2 ** n)
+    matrices_dir = add_graph_dir('WorstCase')
+    for n in range(2, NUMBER_OF_WORST_CASES):
+        gen_worst_case_graph(matrices_dir, 2 ** n)
     print('Worst case graphs generation is finished.')
+
 
 def generate_scale_free_graphs():
     print('Scale free graphs generation is started.')
-    synthetic_dir = os.path.join(DATA_ROOT_DIR, SYNTHETIC_DIR, MATRICES_DIR)
-    os.makedirs(synthetic_dir, exist_ok=True)
-    matrices_dir = os.path.join(synthetic_dir, 'ScaleFree')
-    clean_dir(matrices_dir)
-
+    matrices_dir = add_graph_dir('ScaleFree')
     for k in 1, 3, 5, 10:
         for n in 100, 500, 2500, 10000:
             gen_scale_free_graph(matrices_dir, n, k, ['a', 'b', 'c', 'd'])
     print('Scale free graphs generation is finished.')
 
+
 def gen_sierpinski_graph(target_dir, degree, predicates=['A']):
     ''' Generates a Sierpinski Triangle graph. '''
-    
+
     def sierpinski(t, l, r, deg, preds, g):
         ''' Core function for generating the Sierpinski Triangle. '''
         if deg > 0:
             lt = next(ids)
             tr = next(ids)
             rl = next(ids)
-            sierpinski(l,  lt, rl, deg-1, preds, g)
-            sierpinski(lt, t,  tr, deg-1, preds, g)
-            sierpinski(rl, tr, r,  deg-1, preds, g)
+            sierpinski(l, lt, rl, deg - 1, preds, g)
+            sierpinski(lt, t, tr, deg - 1, preds, g)
+            sierpinski(rl, tr, r, deg - 1, preds, g)
         else:
-            add_edges(l,t,preds,g)
-            add_edges(t,r,preds,g)
-            add_edges(r,l,preds,g)
-            
-    def add_edges(u,v,preds,g):
+            add_edges(l, t, preds, g)
+            add_edges(t, r, preds, g)
+            add_edges(r, l, preds, g)
+
+    def add_edges(u, v, preds, g):
         ''' Adds edges between vertices u and v for all predicates. '''
         for p in preds:
-            g += [[u,p,v]]
-            g += [[v,p,u]]
-            
+            g += [[u, p, v]]
+            g += [[v, p, u]]
+
     def _idgen():
         ''' Generates integer identifiers for vertices. '''
         c = 4
         while True:
             yield c
             c += 1
-            
+
     ids = _idgen()
     graph = []
-    sierpinski(1,2,3,degree,predicates, graph)   
-    with open(os.path.join(target_dir, 'sierpinskigraph_%s.txt'%(degree)), 'w') as out_file:
+    sierpinski(1, 2, 3, degree, predicates, graph)
+    with open(
+        os.path.join(target_dir, 'sierpinskigraph_%s.txt' % (degree)), 'w'
+    ) as out_file:
         for triple in graph:
-            out_file.write('%s %s %s \n'%(triple[0], triple[1], triple[2]))
+            out_file.write('%s %s %s \n' % (triple[0], triple[1], triple[2]))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--update', 
-                    choices=['rdf', 'scalefree', 'full', 'worstcase', 'sparse'],
-                    required=False, type=str, help='partial dataset update')
+    parser.add_argument(
+        '--update',
+        choices=['rdf', 'scalefree', 'full', 'worstcase', 'sparse'],
+        required=False,
+        type=str,
+        help='partial dataset update',
+    )
     args = parser.parse_args()
 
     prt = args.update
 
-    if (prt == 'rdf'):
+    if prt == 'rdf':
         download_data()
         unpack_graphs()
-    elif (prt == 'scalefree'):
+    elif prt == 'scalefree':
         generate_scale_free_graphs()
-    elif (prt == 'full'):
+    elif prt == 'full':
         generate_full_graphs()
-    elif (prt == 'worstcase'):
+    elif prt == 'worstcase':
         generate_worst_case_graphs()
-    elif (prt == 'sparse'):
+    elif prt == 'sparse':
         generate_all_sparse_graphs()
     else:
         install_gtgraph()
