d0d058bb0b1d61abb5a3d72bd4b23046abdc1224
viabzalov <vadim.i.abzalov@gmail.com>
2020-12-05 14:07:25 +0300

    Dataset download refactored

--- cfpq_data.py
+++ cfpq_data.py
@@ -1,10 +1,15 @@
 import argparse
 
+from src.graphs.FullGraph import FullGraph
+from src.graphs.MemoryAliases import MemoryAliases
+from src.graphs.RDF import RDF
+from src.graphs.ScaleFree import ScaleFree
+from src.graphs.SparseGraph import SparseGraph
+from src.graphs.WorstCase import WorstCase
 from src.tools.LUBM.converter import LUBMTool
 from src.tools.gen_RPQ.gen import GenRPQTool
 from src.tools.grammar2cnf.grammar2cnf import Grammar2Cnf
 from src.tools.graph2txt.graph2txt import Graph2TxtTool
-from src.tools.init.init import GraphsLoadTool
 from src.tools.redis_rdf.main import RedisRDFTool
 
 if __name__ == '__main__':
@@ -12,10 +17,15 @@ if __name__ == '__main__':
     subparsers = parser.add_subparsers(dest='tool', required=True)
 
     tools = {
-        'load': GraphsLoadTool()
+        'RDF': RDF()
+        , 'MemoryAliases': MemoryAliases()
+        , 'ScaleFree': ScaleFree()
+        , 'FullGraph': FullGraph()
+        , 'WorstCase': WorstCase()
+        , 'SparseGraph': SparseGraph()
+        , 'LUBM': LUBMTool()
         , 'graph2txt': Graph2TxtTool()
         , 'grammar2cnf': Grammar2Cnf()
-        , 'LUBM': LUBMTool()
         , 'gen_RPQ': GenRPQTool()
         , 'redis_rdf': RedisRDFTool(),
     }
