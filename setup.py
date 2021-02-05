5c7957754080671506eb9f3f565dc5a340536a90
viabzalov <vadim.i.abzalov@gmail.com>
2021-02-05 15:49:19 +0300

    Updated data_files in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cfpq_data_test_dev'
-    , version='1.0.1'
+    name='cfpq_data_test_dev_test'
+    , version='0.0.0'
     , packages=find_packages()
     , url='https://github.com/JetBrains-Research/CFPQ_Data'
     , license='Apache License 2.0'
     , author='Grigorev Semyon'
     , author_email='rsdpisuy@gmail.com'
     , description='Graphs and grammars for experimental analysis of context-free path querying algorithms'
-    # , data_files=[('cfpq_data', ['cfpq_data/release_notes.json'])]
+    , data_files=[('', ['./cfpq_data/release_notes.json'])]
 )
