faddc447d89c0fab353e5f2fc990a577ba86da7f
Nick <Nikitoskova123@gmail.com>
2021-02-08 15:58:28 +0300

    Taxonomy fix

--- tests/RDF/conftest.py
+++ tests/RDF/conftest.py
@@ -8,6 +8,7 @@ rdfs = get_info()['RDF']
 @pytest.fixture(scope='session', params=[
     name
     for name, _ in rdfs.items()
+    if name not in {'taxonomy-hierarchy', 'taxonomy'}
 ])
 def rdf_graph_name(request):
     return request.param
