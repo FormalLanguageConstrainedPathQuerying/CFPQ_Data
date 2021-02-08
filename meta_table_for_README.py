0657795b4f66009eff74b62904075090ba491f55
Nick <Nikitoskova123@gmail.com>
2021-02-08 18:48:49 +0300

    done

--- meta_table_for_README.py
+++ meta_table_for_README.py
@@ -1,55 +1,54 @@
 import json
 import glob
+from cfpq_data.src.utils import get_info
 
-def get_info():
-    with open('./cfpq_data/release_notes.json', 'r') as input_file:
-        data = json.load(input_file)
-        return data
 
 def rdf_dict():
-    data_RDF = dict()
-    names_RDF = get_info()['RDF']
-    for name in names_RDF:
+    data_rdf = dict()
+    names_rdf = get_info()['RDF']
+    for name in names_rdf:
         if name == "taxonomy-hierarchy" or name == "taxonomy":
             continue
         with open(f'./cfpq_data/data/RDF/Graphs/{name}_meta.json', 'r') as graph_info:
-            data_RDF[name] = json.load(graph_info)
-    return data_RDF
+            data_rdf[name] = json.load(graph_info)
+    return data_rdf
 
-def ma_dict():
-    data_MA = dict()
-    names_MA = get_info()['MemoryAliases']
-    for name in names_MA:
-        name_ = glob.glob('./cfpq_data/data/MemoryAliases/Graphs/' + name + '*')[0]
-        with open(name_, 'r') as graph_info:
-            data_MA[name] = json.load(graph_info)
-    return data_MA
+
+def memoryaliases_dict():
+    data_memoryaliases = dict()
+    names_memoryaliases = get_info()['MemoryAliases']
+    for name in names_memoryaliases:
+        name_of_file = glob.glob('./cfpq_data/data/MemoryAliases/Graphs/' + name + '*')[0]
+        with open(name_of_file, 'r') as graph_info:
+            data_memoryaliases[name] = json.load(graph_info)
+    return data_memoryaliases
+
 
 def create_table():
     column_names = ["name", "vertices", "edges", "size of file"]
-    table_RDF = '''
-| Name | Vertices | Edges | Size of file (Bytes) |
-|:---:|:---:|:---:|:---:|
-'''
-    table_MA = '''
-| Name | Vertices | Edges | Size of file (Bytes) |
-|:---:|:---:|:---:|:---:|
-'''
+    table_rdf = \
+        "| Name | Vertices | Edges | Size of file (Bytes) |\n" + \
+        "|:---:|:---:|:---:|:---:|\n"
+
+    table_memoryaliases = \
+        "| Name | Vertices | Edges | Size of file (Bytes) |\n" + \
+        "|:---:|:---:|:---:|:---:|\n"
+
     rdf = rdf_dict()
-    memory_aliases = ma_dict()
+    memory_aliases = memoryaliases_dict()
     names_rdf = get_info()['RDF']
-    names_ma = get_info()['MemoryAliases']
+    names_memoryaliases = get_info()['MemoryAliases']
 
     for name in names_rdf:
         if name == "taxonomy-hierarchy" or name == "taxonomy":
             continue
-        for cname in column_names:
-            table_RDF += "| " + str(rdf[name][cname]) + " "
-        table_RDF += "|\n"
-    for name in names_ma:
-        for cname in column_names:
-            table_MA += "| " + str(memory_aliases[name][cname]) + " "
-        table_MA += "|\n"
+        for column_name in column_names:
+            table_rdf += "| " + str(rdf[name][column_name]) + " "
+        table_rdf += "|\n"
+    for name in names_memoryaliases:
+        for column_name in column_names:
+            table_memoryaliases += "| " + str(memory_aliases[name][column_name]) + " "
+        table_memoryaliases += "|\n"
 
     with open('./docs/README.md', 'rt') as input_file:
         lines = input_file.readlines()
@@ -57,15 +56,16 @@ def create_table():
         for line in lines:
             if "#### RDF" in line:
                 output_file.write(line)
-                output_file.write(table_RDF)
+                output_file.write(table_rdf)
                 output_file.write("\n")
                 continue
             elif "#### MemoryAliases" in line:
                 output_file.write(line)
-                output_file.write(table_MA)
+                output_file.write(table_memoryaliases)
                 output_file.write("\n")
                 continue
             output_file.write(line)
+
 
 def clean_table():
     tmp = 0
@@ -80,7 +80,6 @@ def clean_table():
                 tmp = 0
             if tmp == 0:
                 output_file.write(line)
-
 
 
 clean_table()
