140ed1903038e43c848b5a1d10f1011302d09d7c
Nick <Nikitoskova123@gmail.com>
2021-02-12 17:29:24 +0300

    made test for fullgraph

---
+++ tests/FullGraph/test_fullgraph.py
@@ -0,0 +1,16 @@
+from cfpq_data import full_graph
+import os
+
+def check_metadata(first_graph, second_graph):
+    for field in ['vertices', 'edges']:
+        if first_graph.get_metadata()[field] != second_graph.get_metadata()[field]:
+            return False
+    return True
+
+def test_FullGraph(num_of_vertices):
+        first_graph = full_graph.build(num_of_vertices)
+        second_graph_path = first_graph.save('tmp')
+        second_graph = full_graph.load_from_rdf(second_graph_path)
+        os.remove(second_graph_path)
+
+        assert check_metadata(first_graph, second_graph)
