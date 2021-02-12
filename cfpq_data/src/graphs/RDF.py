119d3c5abcd631507298354f2706068ce8273f55
viabzalov <vadim.i.abzalov@gmail.com>
2021-02-12 19:22:57 +0300

    Added documentation

--- cfpq_data/src/graphs/RDF.py
+++ cfpq_data/src/graphs/RDF.py
@@ -1,5 +1,8 @@
 import json
 import os
+from ctypes import Union
+from pathlib import Path
+from typing import Optional, Tuple, Any, List, Dict
 
 import rdflib
 from tqdm import tqdm
@@ -11,120 +14,235 @@ from cfpq_data.src.utils import download_data, unpack_graph, add_rdf_edge, write
 
 
 class RDF(GraphInterface, CmdParser):
+    """
+    RDF — fixed versions of real-world RDF files (links are provided for updating purposes only!)
+
+    - graphs: already builded graphs
+    - graph_keys: reserved graph names
+    - config: default edge configuration
+    """
+
     graphs = dict()
     graph_keys = RELEASE_INFO['RDF']
     config = RELEASE_INFO['RDF_Config']
 
     def __init__(self):
-        self.type = None
+        """
+        Generic constructor
 
-        self.store = None
+        - type: type of graph instance
+        - store: stored rdflib graph
+        - path: absolute path to graph
+        - dirname: absoute path to graph directory
+        - basename: graph file
+        - vertices_number: number of vertices in the graph
+        - edges_number: number of edges in the graph
+        - file_size: size of graph file, in bytes
+        - file_name: name of graph file
+        - file_extension: extension of graph file
+        """
 
-        self.path = None
+        self.type: Optional[str] = None
+
+        self.store: Optional[rdflib.Graph] = None
+
+        self.path: Optional[Path] = None
         self.dirname = None
         self.basename = None
 
-        self.number_of_vertices = None
-        self.number_of_edges = None
+        self.vertices_number = None
+        self.edges_number = None
 
         self.file_size = None
         self.file_name = None
         self.file_extension = None
 
     @classmethod
-    def build(cls, *args):
+    def build(cls, *args: Union[Path, str]):
+        """
+        An RDF graph builder
+        :param args: only one argument - args[0] - path to graph or reserves graph name
+        :type args: Union[Path, str]
+        :return: RDF graph instance
+        :rtype: RDF
+        """
+
         try:
             return cls.load(args[0])
         except BaseException as ex:
             raise BaseException(f'{cls.__name__}.build: {ex}')
 
     @classmethod
-    def load(cls, path_to_graph=None, file_extension='rdf'):
-        if file_extension == 'txt':
-            rdf_graph = cls.load_from_txt(path_to_graph)
-        else:
-            rdf_graph = cls.load_from_rdf(path_to_graph)
+    def load(cls, source: Optional[Union[Path, str]] = None, source_file_format: str = 'rdf'):
+        """
+        Loads RDF graph from specified source with specified source_file_format
+        :param source: graph source
+        :type source: Optional[Union[Path, str]]
+        :param source_file_format: graph format ('txt'/'rdf')
+        :type source_file_format: str
+        :return: loaded graph
+        :rtype: RDF
+        """
 
-        cls.graphs[(rdf_graph.basename, rdf_graph.file_extension)] = path_to_graph
+        if source_file_format == 'txt':
+            rdf_graph = cls.load_from_txt(source)
+        else:
+            rdf_graph = cls.load_from_rdf(source)
+
+        cls.graphs[(rdf_graph.basename, rdf_graph.file_extension)] = source
 
         return rdf_graph
 
-    def save(self, path_to_graph=None, file_extension='rdf', config=config):
-        if path_to_graph is None:
-            path_to_graph = DATA_FOLDER / self.type / 'Graphs' / self.basename
-        if file_extension == 'txt':
-            self.save_to_txt(path_to_graph, config)
-        else:
-            self.save_to_rdf(path_to_graph)
-        return path_to_graph
+    def save(self
+             , destination: Optional[Union[Path, str]] = None
+             , destination_file_format: str = 'rdf'
+             , config: Dict[str, str] = None) -> Path:
+        """
+        Saves RDF graph to destination with specified destination_file_format and edge configuration
+        :param destination: path to save the graph
+        :type destination: Optional[Union[Path, str]]
+        :param destination_file_format: graph format
+        :type destination_file_format: str
+        :param config: edges configuration
+        :type config: Dict[str, str]
+        :return: path to saved graph
+        :rtype: Path
+        """
 
-    def get_metadata(self):
+        if destination is None:
+            destination = DATA_FOLDER / self.type / 'Graphs' / self.basename
+        if destination_file_format == 'txt':
+            self.save_to_txt(destination, config)
+        else:
+            self.save_to_rdf(destination)
+        return destination
+
+    def get_metadata(self) -> Dict[str, str]:
+        """
+        Generates RDF graph metadata
+        :return: metadata
+        :rtype: Dict[str, str]
+        """
+
         return {
             'name': self.basename
             , 'path': self.path
             , 'version': RELEASE_INFO['version']
-            , 'vertices': self.number_of_vertices
-            , 'edges': self.number_of_edges
+            , 'vertices': self.vertices_number
+            , 'edges': self.edges_number
             , 'size of file': self.file_size
         }
 
-    def save_metadata(self):
-        with open(self.dirname / self.file_name + '_meta.json', 'w') as metadata_file:
+    def save_metadata(self) -> Path:
+        """
+        Saves metadata to specified file
+        :return: path to file with graph metadata
+        :rtype: Path
+        """
+
+        metadata_file_path = self.dirname / self.file_name + '_meta.json'
+
+        with open(metadata_file_path, 'w') as metadata_file:
             json.dump(self.get_metadata(), metadata_file, indent=4)
 
-    def get_triples(self):
+        return metadata_file_path
+
+    def get_triples(self) -> List[Tuple[Any, Any, Any]]:
+        """
+        Returns edges as list of triples (subject, predicate, object)
+        :return: edges
+        :rtype: List[Tuple[Any, Any, Any]]
+        """
+
         triples = list()
+
         for subj, pred, obj in self.store:
             triples.append((subj, pred, obj))
+
         return triples
 
     @classmethod
-    def load_from_rdf(cls, path_to_graph):
-        if hasattr(cls, 'graph_keys') and path_to_graph in cls.graph_keys:
-            graph_name = path_to_graph[:]
+    def load_from_rdf(cls, source: Path = None):
+        """
+        Loads RDF graph from specified source with rdf format
+        :param source: graph source
+        :type source: Path
+        :return: loaded graph
+        :rtype: RDF
+        """
+
+        if hasattr(cls, 'graph_keys') and source in cls.graph_keys:
+            graph_name = source[:]
             download_data(cls.__name__, graph_name, cls.graph_keys[graph_name])
-            path_to_graph = unpack_graph(cls.__name__, graph_name)
+            source = unpack_graph(cls.__name__, graph_name)
 
-        rdf_graph = cls()
+        graph = cls()
 
-        rdf_graph.type = cls.__name__
+        graph.type = cls.__name__
 
-        rdf_graph.store = rdflib.Graph()
-        rdf_graph.store.parse(location=str(path_to_graph), format='xml')
+        graph.store = rdflib.Graph()
+        graph.store.parse(location=str(source), format='xml')
 
-        rdf_graph.path = path_to_graph
-        rdf_graph.dirname = os.path.dirname(path_to_graph)
-        rdf_graph.basename = os.path.basename(path_to_graph)
+        graph.path = source
+        graph.dirname = os.path.dirname(source)
+        graph.basename = os.path.basename(source)
 
-        rdf_graph.number_of_vertices = len(rdf_graph.store.all_nodes())
-        rdf_graph.number_of_edges = len(rdf_graph.store)
+        graph.vertices_number = len(graph.store.all_nodes())
+        graph.edges_number = len(graph.store)
 
-        rdf_graph.file_size = os.path.getsize(path_to_graph)
-        rdf_graph.file_name, rdf_graph.file_extension = os.path.splitext(rdf_graph.basename)
+        graph.file_size = os.path.getsize(source)
+        graph.file_name, graph.file_extension = os.path.splitext(graph.basename)
 
-        return rdf_graph
+        return graph
 
     @classmethod
-    def load_from_txt(cls, path_to_graph):
-        tmp_rdf_graph = rdflib.Graph()
+    def load_from_txt(cls, source: Path = None):
+        """
+        Loads RDF graph from specified source with txt format
+        :param source: graph source
+        :type source: Path
+        :return: loaded graph
+        :rtype: RDF
+        """
 
-        with open(path_to_graph, 'r') as input_file:
+        tmp_graph = rdflib.Graph()
+
+        with open(source, 'r') as input_file:
             for edge in input_file:
                 s, p, o = edge.split()
-                add_rdf_edge(s, p, o, tmp_rdf_graph)
-        write_to_rdf('tmp.xml', tmp_rdf_graph)
+                add_rdf_edge(s, p, o, tmp_graph)
 
-        rdf_graph = cls.load_from_rdf('tmp.xml')
+        write_to_rdf('tmp.xml', tmp_graph)
+
+        graph = cls.load_from_rdf(Path('tmp.xml'))
 
         os.remove('tmp.xml')
 
-        return rdf_graph
+        return graph
 
-    def save_to_rdf(self, path_to_graph):
-        write_to_rdf(path_to_graph, self.store)
-        return path_to_graph
+    def save_to_rdf(self, destination: Path):
+        """
+        Saves RDF graph to destination rdf file
+        :param destination: path to save the graph
+        :type destination: Path
+        :return: path to saved graph
+        :rtype: Path
+        """
 
-    def save_to_txt(self, path_to_graph, config=config):
+        write_to_rdf(Path, self.store)
+        return destination
+
+    def save_to_txt(self, destination: Path, config: Dict[str, str] = config):
+        """
+        Saves RDF graph to destination txt file with specified edge configuration
+        :param destination: path to save the graph
+        :type destination: Path
+        :param config: edges configuration
+        :type config: Dict[str, str]
+        :return: path to saved graph
+        :rtype: Path
+        """
+
         vertices = dict()
         edges = dict()
         next_id = 0
@@ -151,14 +269,22 @@ class RDF(GraphInterface, CmdParser):
                 , vertices[obj]
             ))
 
-        with open(path_to_graph, 'w') as output_file:
+        with open(destination, 'w') as output_file:
             for s, p, o in triples:
                 output_file.write(f'{s} {p} {o}\n')
 
-        return path_to_graph
+        return destination
 
     @staticmethod
     def init_cmd_parser(parser):
+        """
+        Initialize command line parser
+        :param parser: RDF subparser of command line parser
+        :type parser: ArgumentParser
+        :return: None
+        :rtype: None
+        """
+
         parser.add_argument(
             '-a'
             , '--all'
@@ -176,6 +302,14 @@ class RDF(GraphInterface, CmdParser):
 
     @staticmethod
     def eval_cmd_parser(args):
+        """
+        Evaluate command line parser
+        :param args: command line arguments
+        :type args: Namespace
+        :return: None
+        :rtype: None
+        """
+
         if args.all is False and args.graph is None:
             print('One of -a/--all, -g/--graph required')
             exit()
