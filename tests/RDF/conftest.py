6862b51f425ebc46aa22bbb28e7f873dd6a0fd40
viabzalov <vadim.i.abzalov@gmail.com>
2021-02-12 21:45:38 +0300

    Fixes

--- tests/RDF/conftest.py
+++ tests/RDF/conftest.py
@@ -1,16 +1,22 @@
 import pytest
 
 from cfpq_data.config import RELEASE_INFO
-from cfpq_data.src.utils import get_graph_info
 
 rdfs = RELEASE_INFO['RDF']
 
 
 @pytest.fixture(scope='session', params=[
-    name
-    for name, _ in rdfs.items()
-    if name not in {'taxonomy-hierarchy', 'taxonomy'}
-    if get_graph_info('RDF', name)['size of file'] < 1e7
+    'atom-primitive'
+    , 'biomedical-mesure-primitive'
+    , 'core'
+    , 'foaf'
+    , 'funding'
+    , 'generations'
+    , 'people_pets'
+    , 'skos'
+    , 'travel'
+    , 'univ-bench'
+    , 'wine'
 ])
 def rdf_graph_name(request):
     return request.param
