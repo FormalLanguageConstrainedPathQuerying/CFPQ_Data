cee7da83961f6871ef29d28341ca503ce1c774c4
Nick <Nikitoskova123@gmail.com>
2021-02-12 17:30:11 +0300

    updated tests for rdf and memoryaliases

--- tests/MemoryAliases/test_memoryaliases_graph.py
+++ tests/MemoryAliases/test_memoryaliases_graph.py
@@ -1,4 +1,5 @@
 from cfpq_data import memory_aliases_graph
+import os
 
 def check_metadata(first_graph, second_graph):
     for field in ['vertices', 'edges']:
@@ -7,8 +8,12 @@ def check_metadata(first_graph, second_graph):
     return True
 
 def test_graph(ma_graph_name):
-    first_graph = memory_aliases_graph.build(ma_graph_name)
+    first_graph = memory_aliases_graph.build(ma_graph_name).save('tmp1')
+    first_graph_obj = memory_aliases_graph.build(first_graph)
+    second_graph = memory_aliases_graph.build(ma_graph_name).save('tmp2')
+    second_graph_obj = memory_aliases_graph.build(second_graph)
 
-    second_graph = memory_aliases_graph.build(ma_graph_name)
+    os.remove(first_graph)
+    os.remove(second_graph)
 
-    assert check_metadata(first_graph, second_graph)
+    assert check_metadata(first_graph_obj, second_graph_obj)
