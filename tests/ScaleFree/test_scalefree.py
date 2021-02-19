b9a8fb247669be372564437be7dd398e8e22300d
Nick <Nikitoskova123@gmail.com>
2021-02-19 17:55:24 +0300

    added txt tests

--- tests/ScaleFree/test_scalefree.py
+++ tests/ScaleFree/test_scalefree.py
@@ -18,3 +18,14 @@ def test_scalefree(suit_scalefree):
     os.remove(second_graph_path)
 
     assert check_metadata(first_graph, second_graph)
+
+
+def test_txt(suit_scalefree):
+    vertices_number, vertices_degree = suit_scalefree
+    first_graph = ScaleFree.build(vertices_number, vertices_degree)
+    first_graph_path = first_graph.save('tmp1', 'txt')
+    second_graph = ScaleFree.load_from_txt(first_graph_path)
+
+    os.remove(first_graph_path)
+
+    assert check_metadata(first_graph, second_graph)
