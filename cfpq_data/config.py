838a1a56dfd7140345bf9f5cd6d51d1928aa56e6
viabzalov <vadim.i.abzalov@gmail.com>
2021-02-19 21:27:14 +0300

    Fixed config.py

--- cfpq_data/config.py
+++ cfpq_data/config.py
@@ -9,7 +9,3 @@ with open(MAIN_FOLDER / 'release_notes.json', 'r') as input_file:
     RELEASE_INFO = json.load(input_file)
 
 PACKAGE_VERSION = RELEASE_INFO['version']
-
-RDF_CONFIG = RELEASE_INFO['RDF_Config']
-MEMORYALIASES_CONFIG = RELEASE_INFO['MemoryAliases_Config']
-GENERATORS_CONFIG = RELEASE_INFO['Generators_Config']
