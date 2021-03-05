b0990adad253b64d0031e8f009ae9fe6290f4df8
Nick <Nikitoskova123@gmail.com>
2021-03-05 18:45:59 +0300

    updated blanks

--- tests/Grammars/test_grammar.py
+++ tests/Grammars/test_grammar.py
@@ -1,21 +1,26 @@
 from cfpq_data import Grammar, CNFGrammar
 from pyformlang.cfg import *
 
-def test_load_Grammar(grammar_name):
+
+def test_load_grammar(grammar_name):
     assert Grammar.load_from_txt(grammar_name)
 
-def test_load_CNFGrammar(grammar_name):
+
+def test_load_cnfgrammar(grammar_name):
     assert CNFGrammar.load_from_txt(grammar_name)
 
-def test_grammar_from_Grammar(grammar_name):
+
+def test_grammar_from_grammar(grammar_name):
     cfg = Grammar.load_from_txt(grammar_name)
     assert CNFGrammar.from_grammar(cfg)
 
-def test_grammar_from_CNFGrammar(grammar_name):
+
+def test_grammar_from_cnfgrammar(grammar_name):
     cfg = CNFGrammar.load_from_txt(grammar_name)
     assert Grammar.from_grammar(cfg)
 
-def test_CNF(grammar_name):
+
+def test_cnf(grammar_name):
     cfg = Grammar.load_from_txt(grammar_name)
     cfg = CNFGrammar.from_grammar(cfg)
     cfg = CFG(
@@ -29,10 +34,12 @@ def test_CNF(grammar_name):
         cfg.productions.remove(Production(cfg.start_symbol, []))
     assert cfg.is_normal_form()
 
-def test_dump_Grammar(grammar_name):
+
+def test_dump_grammar(grammar_name):
     cfg = Grammar.load_from_txt(grammar_name)
     assert cfg.dump_to_txt('tmp1.txt')
 
-def test_dump_CNFGrammar(grammar_name):
+
+def test_dump_cnfgrammar(grammar_name):
     cfg = CNFGrammar.load_from_txt(grammar_name)
     assert cfg.dump_to_txt('tmp2.txt')
