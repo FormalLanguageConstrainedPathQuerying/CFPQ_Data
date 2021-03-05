8751b490d0589c1a0f33e32ec2fb374f5c6ae698
Nick <Nikitoskova123@gmail.com>
2021-03-05 16:19:56 +0300

    added grammars tests

--- tests/Grammars/conftest.py
+++ tests/Grammars/conftest.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+
 import pytest
 import os
 
-PATH_FOLDER = '../cfpq_data/data/'
+PATH_FOLDER = Path(__file__).parent.parent.parent / 'cfpq_data' / 'data'
 grammar_list = os.listdir(PATH_FOLDER)
+
 
 @pytest.fixture(scope='session', params=[name for name in grammar_list if name.endswith(".txt")])
 def grammar_name(request):
