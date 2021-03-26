c79e95f866e8de9a0b3a2856f092092e8416561e
Vadim Abzalov <vadim.i.abzalov@gmail.com>
2021-03-26 20:24:49 +0300

    Updated package structure

---
+++ cfpq_data/graphs/creators/__init__.py
@@ -0,0 +1,6 @@
+from cfpq_data.graphs.creators.graph_creator import *
+from cfpq_data.graphs.creators.barabasi_albert_graph_creator import *
+from cfpq_data.graphs.creators.cycle_graph_creator import *
+from cfpq_data.graphs.creators.rdf_graph_creator import *
+from cfpq_data.graphs.creators.scale_free_graph_creator import *
+from cfpq_data.graphs.creators.two_cycles_grap_creator import *
