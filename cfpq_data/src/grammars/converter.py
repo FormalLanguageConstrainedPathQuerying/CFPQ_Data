3fd2e8972c504aebf73bbf43104e4fdf5f8bbfdf
Nick <Nikitoskova123@gmail.com>
2021-03-26 17:00:06 +0300

    updating structure. second step

---
+++ cfpq_data/src/grammars/converter.py
@@ -0,0 +1,96 @@
+from abc import abstractmethod, ABC
+from cfpq_data.src.grammars.grammar import CNFGrammar, BaseGrammar, RSA
+from cfpq_data.src.grammars.creator import TXTRSAGrammarCreator
+
+
+class GrammarConverter(ABC):
+
+    @abstractmethod
+    def convert(self):
+        pass
+
+
+class BaseGrammarToCNFGrammarConverter(GrammarConverter):
+
+    def __init__(self, grammar_obj: BaseGrammar):
+        self.grammar = grammar_obj
+
+    def convert(self) -> CNFGrammar:
+        """
+        Converting BaseGrammar into CNF
+
+        :return: CNFGrammar object
+        """
+        source = self.grammar
+        return CNFGrammar(
+            variables=source.variables,
+            terminals=source.terminals,
+            start_symbol=source.start_symbol,
+            productions=source.productions
+        )
+
+
+class CNFGrammarToBaseGrammarConverter(GrammarConverter):
+
+    def __init__(self, grammar_obj: CNFGrammar):
+        self.grammar = grammar_obj
+
+    def convert(self) -> BaseGrammar:
+        """
+        Converting CNF into BaseGrammar
+
+        :return: BaseGrammar object
+        """
+        source = self.grammar
+        return BaseGrammar(
+            variables=source.variables,
+            terminals=source.terminals,
+            start_symbol=source.start_symbol,
+            productions=source.productions
+        )
+
+
+class BaseGrammarToRSAConverter(GrammarConverter):
+
+    def __init__(self, grammar_obj: BaseGrammar):
+        self.grammar = grammar_obj
+
+    def convert(self) -> RSA:
+        """
+        Converting BaseGrammar into RSA
+
+        :return: RSA object
+        """
+        grammar_obj = self.grammar
+        grammar_obj.dump_to_txt('tmp.txt')
+        g = TXTRSAGrammarCreator('tmp.txt', grammar_obj.start_symbol).create()
+        return g
+
+
+class CNFGrammarToRSAConverter(GrammarConverter):
+
+    def __init__(self, grammar_obj: CNFGrammar):
+        self.grammar = grammar_obj
+
+    def convert(self) -> RSA:
+        """
+        Converting CNF into RSA
+
+        :return: RSA object
+        """
+        grammar_obj = self.grammar
+        grammar_obj.dump_to_txt('tmp.txt')
+        g = TXTRSAGrammarCreator('tmp.txt', grammar_obj.start_symbol).create()
+        return g
+
+
+class RSAToCNFGrammarConverter(GrammarConverter):
+
+    def convert(self):
+        pass
+
+
+class RSAToBaseGrammarConverter(GrammarConverter):
+
+    def convert(self):
+        pass
---
+++ cfpq_data/src/grammars/serializer.py
@@ -0,0 +1,67 @@
+from abc import abstractmethod, ABC
+from pyformlang.cfg import *
+from cfpq_data.src.grammars.grammar import CNFGrammar, BaseGrammar
+
+
+class GrammarSerializer(ABC):
+
+    @abstractmethod
+    def dump(self):
+        pass
+
+
+class BaseGrammarToTXT(GrammarSerializer):
+
+    def __init__(self, grammar_obj: BaseGrammar, path: str):
+        self.path = path
+        self.grammar = grammar_obj
+
+    def dump(self) -> str:
+        """
+        Saving BaseGrammar to txt file
+
+        :return: path
+        """
+        path = self.path
+        source = self.grammar
+
+        cfg = CFG(
+            variables=source.variables,
+            terminals=source.terminals,
+            start_symbol=source.start_symbol,
+            productions=source.productions
+        )
+        with open(path, 'w') as f:
+            f.write(cfg.to_text())
+        return path
+
+
+class CNFGrammarToTXT(GrammarSerializer):
+
+    def __init__(self, grammar_obj: CNFGrammar, path: str):
+        self.path = path
+        self.grammar = grammar_obj
+
+    def dump(self) -> str:
+        """
+        Saving CNFGrammar to txt file
+
+        :return: path
+        """
+        path = self.path
+        source = self.grammar
+
+        cfg = CFG(
+            variables=source.variables,
+            terminals=source.terminals,
+            start_symbol=source.start_symbol,
+            productions=source.productions
+        )
+        with open(path, 'w') as f:
+            f.write(cfg.to_text())
+        return path
+
+
+class RSAToTXT(GrammarSerializer):
+
+    pass
