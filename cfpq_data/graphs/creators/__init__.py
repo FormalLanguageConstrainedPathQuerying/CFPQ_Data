2760acba92bb541b702e8ddc01be7e1adc0bd2db
Vadim Abzalov <vadim.i.abzalov@gmail.com>
2021-04-02 19:01:59 +0300

    Updated project structure

--- cfpq_data/graphs/creators/__init__.py
+++ cfpq_data/graphs/creators/__init__.py
@@ -1,9 +1,8 @@
-from cfpq_data.graphs.creators.graph_creator import *
 from cfpq_data.graphs.creators.barabasi_albert_graph_creator import *
+from cfpq_data.graphs.creators.binomial_graph_creator import *
 from cfpq_data.graphs.creators.cycle_graph_creator import *
+from cfpq_data.graphs.creators.fast_binomial_graph_creator import *
+from cfpq_data.graphs.creators.graph_creator import *
 from cfpq_data.graphs.creators.rdf_graph_creator import *
 from cfpq_data.graphs.creators.scale_free_graph_creator import *
-from cfpq_data.graphs.creators.two_cycles_grap_creator import *
-
-from cfpq_data.graphs.serializers.graph_serializer import *
-from cfpq_data.graphs.serializers.TXTGraphSerializer import *
+from cfpq_data.graphs.creators.two_cycles_graph_creator import *
