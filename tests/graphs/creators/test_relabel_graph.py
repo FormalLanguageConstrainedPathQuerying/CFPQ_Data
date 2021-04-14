50dfac15318265fa3b14c0a65061ac92f849f51a
vdshk <vadim.i.abzalov@gmail.com>
2021-04-14 20:22:34 +0300

    Revised TestSpecifiedGraphCreator

---
+++ tests/graphs/creators/test_relabel_graph.py
@@ -0,0 +1,30 @@
+import random
+
+import numpy as np
+import pytest
+
+import cfpq_data
+
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+
+g1 = cfpq_data.labeled_barabasi_albert_graph(42, 3, seed, edge_labels="A")
+g2 = cfpq_data.labeled_binomial_graph(42, 0.73, seed, edge_labels="A")
+
+rg1 = cfpq_data.relabel_graph(g1, {"A": "B"})
+rg2 = cfpq_data.relabel_graph(g2, {"A": "B"})
+
+
+@pytest.mark.parametrize(
+    "graph,expected_nodes,expected_edges",
+    [
+        (rg1, g1.number_of_nodes(), g1.number_of_edges()),
+        (rg2, g2.number_of_nodes(), g2.number_of_edges()),
+    ],
+)
+def test_relabel_graph(graph, expected_nodes, expected_edges):
+    assert (
+        graph.number_of_nodes() == expected_nodes
+        and graph.number_of_edges() == expected_edges
+    )
