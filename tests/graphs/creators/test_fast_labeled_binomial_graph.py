21f1a862fae7039368729d56b1a27c5250b5e9c1
vdshk <vadim.i.abzalov@gmail.com>
2021-04-14 20:21:20 +0300

    Revised TestFastBinomialGraphCreator

---
+++ tests/graphs/creators/test_fast_labeled_binomial_graph.py
@@ -0,0 +1,23 @@
+import random
+
+import numpy as np
+import pytest
+
+import cfpq_data
+
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+
+g1 = cfpq_data.fast_labeled_binomial_graph(29, 0.1, seed=seed)
+g2 = cfpq_data.fast_labeled_binomial_graph(42, 0.1, seed=seed)
+
+
+@pytest.mark.parametrize(
+    "graph,expected_nodes,expected_edges", [(g1, 29, 91), (g2, 42, 182)]
+)
+def test_fast_labeled_binomial_graph(graph, expected_nodes, expected_edges):
+    assert (
+        graph.number_of_nodes() == expected_nodes
+        and graph.number_of_edges() == expected_edges
+    )
