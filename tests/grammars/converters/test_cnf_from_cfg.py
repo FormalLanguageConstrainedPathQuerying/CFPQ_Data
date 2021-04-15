39b14e4f29607880ef6f51bd0d14d784d3bd8a22
Nick <Nikitoskova123@gmail.com>
2021-04-15 20:16:32 +0300

    tests for grammars

---
+++ tests/grammars/converters/test_cnf_from_cfg.py
@@ -0,0 +1,19 @@
+import pytest
+import cfpq_data
+
+
+cfg_1 = cfpq_data.cfg_from_txt("S -> a S b | a b")
+cnf_1 = cfpq_data.cnf_from_cfg(cfg=cfg_1)
+
+cfg_2 = cfpq_data.cfg_from_txt("S -> sco_r S sco | t_r S t | sco_r sco | t_r t")
+cnf_2 = cfpq_data.cnf_from_cfg(cfg=cfg_2)
+
+
+@pytest.mark.parametrize(
+    "grammar_1, grammar_2", [(cnf_1, cnf_2)]
+)
+def test_cfg_from_cnf(grammar_1, grammar_2):
+    assert (
+        grammar_1.contains(["a", "b"])
+        and grammar_2.contains(["sco_r", "sco"])
+    )
---
+++ tests/grammars/creators/test_rsm_from_txt.py
@@ -0,0 +1,17 @@
+import pytest
+import cfpq_data
+
+
+rsm_1 = cfpq_data.rsm_from_txt("S -> a S b | a b")
+
+rsm_2 = cfpq_data.rsm_from_txt("S -> sco_r S sco | t_r S t | sco_r sco | t_r t")
+
+
+@pytest.mark.parametrize(
+    "grammar_1, grammar_2", [(rsm_1, rsm_2)]
+)
+def test_cfg_from_cnf(grammar_1, grammar_2):
+    assert (
+        grammar_1.contains(["a", "b"])
+        and grammar_2.contains(["sco_r", "sco"])
+    )
