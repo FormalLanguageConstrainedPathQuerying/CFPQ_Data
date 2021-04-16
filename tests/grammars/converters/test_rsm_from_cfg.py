5216369a3c30b991f042d282513ae123794bb17c
Nick <Nikitoskova123@gmail.com>
2021-04-16 11:55:40 +0300

    updated code style

--- tests/grammars/converters/test_rsm_from_cfg.py
+++ tests/grammars/converters/test_rsm_from_cfg.py
@@ -9,11 +9,6 @@ cfg_2 = cfpq_data.cfg_from_txt("S -> sco_r S sco | t_r S t | sco_r sco | t_r t")
 rsm_2 = cfpq_data.rsm_from_cfg(cfg=cfg_2)
 
 
-@pytest.mark.parametrize(
-    "grammar_1, grammar_2", [(rsm_1, rsm_2)]
-)
+@pytest.mark.parametrize("grammar_1, grammar_2", [(rsm_1, rsm_2)])
 def test_cfg_from_cnf(grammar_1, grammar_2):
-    assert (
-        grammar_1.contains(["a", "b"])
-        and grammar_2.contains(["sco_r", "sco"])
-    )
+    assert grammar_1.contains(["a", "b"]) and grammar_2.contains(["sco_r", "sco"])
--- tests/grammars/creators/test_cnf_from_txt.py
+++ tests/grammars/creators/test_cnf_from_txt.py
@@ -7,11 +7,6 @@ cnf_1 = cfpq_data.cnf_from_txt("S -> a S b | a b")
 cnf_2 = cfpq_data.cnf_from_txt("S -> sco_r S sco | t_r S t | sco_r sco | t_r t")
 
 
-@pytest.mark.parametrize(
-    "grammar_1, grammar_2", [(cnf_1, cnf_2)]
-)
+@pytest.mark.parametrize("grammar_1, grammar_2", [(cnf_1, cnf_2)])
 def test_cfg_from_cnf(grammar_1, grammar_2):
-    assert (
-        grammar_1.contains(["a", "b"])
-        and grammar_2.contains(["sco_r", "sco"])
-    )
+    assert grammar_1.contains(["a", "b"]) and grammar_2.contains(["sco_r", "sco"])
