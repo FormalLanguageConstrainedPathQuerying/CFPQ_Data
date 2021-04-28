544eca4234aaeeb787ebde804d235691c746b0fe
vdshk <vadim.i.abzalov@gmail.com>
2021-04-28 14:25:39 +0300

    Added graph generators

---
+++ cfpq_data/graphs/generators/__init__.py
@@ -0,0 +1,6 @@
+from cfpq_data.graphs.generators.fast_labeled_binomial_graph import *
+from cfpq_data.graphs.generators.labeled_barabasi_albert_graph import *
+from cfpq_data.graphs.generators.labeled_binomial_graph import *
+from cfpq_data.graphs.generators.labeled_cycle_graph import *
+from cfpq_data.graphs.generators.labeled_scale_free_graph import *
+from cfpq_data.graphs.generators.labeled_two_cycles_graph import *
