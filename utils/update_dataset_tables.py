19bbc66f480489ea959714e0e2be939d676547b3
vdshk <vadim.i.abzalov@gmail.com>
2021-05-09 16:53:09 +0300

    Revised update_dataset_tables.py

--- utils/update_dataset_tables.py
+++ utils/update_dataset_tables.py
@@ -1,35 +1,49 @@
-from csv import DictWriter
+from csv import DictWriter, QUOTE_MINIMAL
 
 from cfpq_data import graph_from_dataset
+from cfpq_data.dataset import DATASET
 from config import MAIN_FOLDER
-from fetch_dataset import fetch_dataset
 
 
 def update_dataset_tables(dataset):
     for graph_class in dataset.keys():
+        table = list()
         fieldnames = ["Graph", "#Vertices", "#Edges"]
+
+        for graph_name in dataset[graph_class]:
+            graph = graph_from_dataset(graph_name)
+            table.append(
+                [
+                    graph_name,
+                    f"{graph.number_of_nodes():,}",
+                    f"{graph.number_of_edges():,}",
+                ]
+            )
+
+        table.sort(key=lambda row: int(row[2].replace(",", "")))
+
         with open(
             MAIN_FOLDER / "docs" / "dataset" / f"{graph_class}.csv", mode="w"
         ) as csv_file:
-            csv_writer = DictWriter(csv_file, fieldnames=fieldnames)
+            csv_writer = DictWriter(
+                csv_file,
+                fieldnames=fieldnames,
+                delimiter=";",
+                quotechar='"',
+                quoting=QUOTE_MINIMAL,
+            )
             csv_writer.writeheader()
 
-            for graph_name in dataset[graph_class]:
-                graph = graph_from_dataset(graph_name)
+            for row in table:
                 csv_writer.writerow(
                     dict(
                         zip(
                             fieldnames,
-                            [
-                                graph_name,
-                                f"{graph.number_of_nodes():,}",
-                                f"{graph.number_of_edges():,}",
-                            ],
+                            row,
                         )
                     )
                 )
 
 
 if __name__ == "__main__":
-    dataset = fetch_dataset()
-    update_dataset_tables(dataset)
+    update_dataset_tables(DATASET)
