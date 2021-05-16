75a6c67d97a10f86c61f5fc6d7095eda5bbb3134
vdshk <vadim.i.abzalov@gmail.com>
2021-05-16 23:06:08 +0300

    Added tests for txt.py

--- tests/graphs/readwrite/txt.py
+++ tests/graphs/readwrite/txt.py
@@ -13,7 +13,7 @@ np.random.seed(seed)
 
 g1 = cfpq_data.labeled_binomial_graph(42, 0.42, seed=seed, verbose=False)
 g2 = cfpq_data.labeled_binomial_graph(42, 0.73, seed=seed, verbose=False)
-g3 = cfpq_data.graph_from_text("1 2\n2 3", verbose=False)
+g3 = cfpq_data.graph_from_text("1 2", verbose=False)
 
 
 @pytest.mark.parametrize(
@@ -28,6 +28,29 @@ def test_txt(graph):
     (fd, fname) = tempfile.mkstemp()
 
     path = cfpq_data.graph_to_txt(graph, fname, verbose=False)
+    gin = cfpq_data.graph_from_txt(path, verbose=False)
+
+    os.close(fd)
+    os.unlink(fname)
+
+    assert (
+        graph.number_of_nodes() == gin.number_of_nodes()
+        and graph.number_of_edges() == gin.number_of_edges()
+    )
+
+
+@pytest.mark.parametrize(
+    "graph",
+    [
+        g1,
+        g2,
+        g3,
+    ],
+)
+def test_txt_with_quoting(graph):
+    (fd, fname) = tempfile.mkstemp()
+
+    path = cfpq_data.graph_to_txt(graph, fname, quoting=True, verbose=False)
     gin = cfpq_data.graph_from_txt(path, verbose=False)
 
     os.close(fd)
