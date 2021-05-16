9bf01f3535da6c69fc495a8b0d5ae633bee8b1cb
vdshk <vadim.i.abzalov@gmail.com>
2021-05-16 23:01:32 +0300

    Added tests for rdf.py

--- tests/graphs/readwrite/rdf.py
+++ tests/graphs/readwrite/rdf.py
@@ -27,3 +27,26 @@ def test_rdf(graph_name):
         graph.number_of_nodes() == gin.number_of_nodes()
         and graph.number_of_edges() == gin.number_of_edges()
     )
+
+
+@pytest.mark.parametrize(
+    "graph_name",
+    [
+        "people_pets",
+        "foaf",
+    ],
+)
+def test_rdf_with_verbose(graph_name):
+    (fd, fname) = tempfile.mkstemp()
+
+    graph = cfpq_data.graph_from_dataset(graph_name)
+    path = cfpq_data.graph_to_rdf(graph, fname)
+    gin = cfpq_data.graph_from_rdf(path)
+
+    os.close(fd)
+    os.unlink(fname)
+
+    assert (
+        graph.number_of_nodes() == gin.number_of_nodes()
+        and graph.number_of_edges() == gin.number_of_edges()
+    )
