60140b38fc2e76b52c82f38801080ad97a0522a5
vdshk <vadim.i.abzalov@gmail.com>
2021-08-16 15:49:33 +0300

    Added tests for RSA converters

--- tests/grammars/converters/test_rsa_converters.py
+++ tests/grammars/converters/test_rsa_converters.py
@@ -1,4 +1,3 @@
-# TODO: Remove in cfpq_data 2.0.0
 import pytest
 
 import cfpq_data
@@ -11,14 +10,16 @@ cnf_2 = cfpq_data.cnf_from_text("S -> sco_r S sco | t_r S t | sco_r sco | t_r t"
 
 
 @pytest.mark.parametrize("cfg", [cfg_1, cfg_2])
-def test_rsm_from_cfg(cfg):
-    rsm = cfpq_data.rsm_from_cfg(cfg)
+def test_rsa_from_cfg(cfg):
+    rsa = cfpq_data.rsa_from_cfg(cfg)
+    cfg_from_rsa = cfpq_data.cfg_from_rsa(rsa)
     for word in cfg.get_words(4):
-        assert rsm.contains(word) and cfg.contains(word)
+        assert cfg_from_rsa.contains(word) and cfg.contains(word)
 
 
 @pytest.mark.parametrize("cnf", [cnf_1, cnf_2])
-def test_rsm_from_cnf(cnf):
-    rsm = cfpq_data.rsm_from_cnf(cnf)
+def test_rsa_from_cnf(cnf):
+    rsa = cfpq_data.rsa_from_cnf(cnf)
+    cfg_from_rsa = cfpq_data.cfg_from_rsa(rsa)
     for word in cnf.get_words(4):
-        assert rsm.contains(word) and cnf.contains(word)
+        assert cfg_from_rsa.contains(word) and cnf.contains(word)
