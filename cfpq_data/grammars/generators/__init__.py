4b8e8f298695ff1c11da572c55cf177f9cd44bbf
rustam-azimov <rustam.azimov19021995@gmail.com>
2022-11-14 18:14:27 +0300

    Added C alias generator.

--- cfpq_data/grammars/generators/__init__.py
+++ cfpq_data/grammars/generators/__init__.py
@@ -1,2 +1,3 @@
+from cfpq_data.grammars.generators.c_alias_grammar import *
 from cfpq_data.grammars.generators.dyck_grammar import *
 from cfpq_data.grammars.generators.nested_parentheses_grammar import *
